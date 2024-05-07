2a1079d7b640291a83ce5649f951abef18e39259
Mohammed Yousef <mohammedmohammeddeeb@gmail.com>
2024-05-07 03:58:44 +0300

    Fix getUserById endpoint, Add the database Get the data from db

--- Blu-training2/src/main/java/com/blu/app/api/controller/UserController.java
+++ Blu-training2/src/main/java/com/blu/app/api/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -35,18 +36,9 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<?> getUserById(@PathVariable int id) {
-        try {
-            User user = userService.getUserById(id);
-            if (user == null) {
-                return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<User>(user, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<String>("Internal Server Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }    }
+    public User getUserById(@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
 
 
 }
--- Blu-training2/src/main/java/com/blu/app/service/UserService.java
+++ Blu-training2/src/main/java/com/blu/app/service/UserService.java
@@ -1,43 +1,38 @@
 package com.blu.app.service;
 
 import com.blu.app.api.model.User;
+import com.blu.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    private List<User> users;
+    @Autowired
+    private UserRepository userRepository;
 
     public UserService(){
-        users = new ArrayList<User>();
-
-        User user1 = new User(1,"Ahmad", 18, "ahmad@gmail.com");
-        User user2 = new User(2,"Mohammed", 26, "mohammed@gmail.com");
-        User user3 = new User(3,"Hasan", 23, "hasan@gmail.com");
-        User user4 = new User(4,"Khaled", 30, "khaled@gmail.com");
-        User user5 = new User(5,"Ali", 25, "ali@gmail.com");
-        users.addAll(Arrays.asList(user1,user2,user3,user4,user5));
     }
+
+
 
     public List<User> getUsers() {
-        if(users.isEmpty()){
-            return null;
-        }
-        return users;
+        return userRepository.findAll();
     }
 
-    public User getUserById(int id){
-        for (User user: users) {
-            if(id == user.getId()){
-                return user;
-
-            }
+    public User getUserById(Integer id){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isEmpty()){
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.NOT_FOUND, "Book not found");
         }
-        return null;
+        return userOptional.get();
 
     }
 }
